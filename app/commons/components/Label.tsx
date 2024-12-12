c0e9ff147f11ce5988af63feb2df55d8adadafb6
Krzysztof Adamczak <krzadamczak@gmail.com>
2024-12-12 21:38:10 +0100

    global gitignore

--- app/commons/components/Label.tsx
+++
@@ -1,7 +0,0 @@
-import type { ComponentPropsWithRef } from 'react';
-
-type LabelProps = ComponentPropsWithRef<'label'>;
-
-export const Label = ({ children, ...props }: LabelProps) => {
-    return <label {...props}>{children}</label>;
-};
